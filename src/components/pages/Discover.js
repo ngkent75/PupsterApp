9d3a5fed1e602ee4b9821ace69dd99dc3480ad90
lauracejas <lauracejasmartin94@gmail.com>
2021-04-09 11:31:19 -0400

    first commit

---
+++ src/components/pages/Discover.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Discover() {
+  return (
+    <div>
+      <h1>Make New Friends</h1>
+      <p>
+      Thumbs up on any pups you'd like to meet!
+      </p>
+    </div>
+  );
+}
+
+export default Discover;
